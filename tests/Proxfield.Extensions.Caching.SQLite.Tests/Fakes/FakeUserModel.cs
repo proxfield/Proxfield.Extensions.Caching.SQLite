88a04a5244bdc3c0d6b2d55ef54a7da04b43ad99
Marco Lima <marckdx@outlook.com>
2022-07-12 21:16:03 -0300

    removing soon to be new implementations

--- tests/Proxfield.Extensions.Caching.SQLite.Tests/Fakes/FakeUserModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Proxfield.Extensions.Caching.SQLite.Tests.Mocks
-{
-    public class FakeUserModel : Cacheable
-    {
-        public FakeUserModel(ISQLiteCache cache) : base(cache)
-        {
-        }
-    }
-}
